8fa82254aed007ce187402d3f1dc1303f2c8a405
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-01-01 18:30:20 +0500

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -17,21 +17,6 @@ module.exports = ({ env }) => ({
         defaultReplyTo: "no-reply@devmirza.ml",
       },
     },
-  },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+  }
   // ...
 });
