9afe099963de6569b77a0bafaaea430779f551fa
DevMirza <53424436+Zaid-maker@users.noreply.github.com>
2023-01-01 23:15:13 +0500

    Update plugins.js

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -18,19 +18,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
-    },
-  },
 });
